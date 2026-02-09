684afa85f23883ee7a1bd2268248d4178314bd44
Kostiantyn <phlyze@gmail.com>
2026-02-09 22:42:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -506,7 +506,7 @@ const mono = [
 
 const additions = [
   {
-    name: 'Macaroon (minimum - 3 pcs.)',
+    name: 'Macaroon (min - 3 pcs.)',
     image: 'https://storage.googleapis.com/floralis/2026/14-feb/additions/1.jpeg',
     sizes: [
       {
